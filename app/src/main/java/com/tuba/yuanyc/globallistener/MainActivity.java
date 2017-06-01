124dc930a2fddcd98077080aeb17d1cc36874740
yuanyc <yuanyc@mapbar.com>
2017-06-01 10:30:50 +0800

    chg 第一次提交本地

---
+++ app/src/main/java/com/tuba/yuanyc/globallistener/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tuba.yuanyc.globallistener;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //第一次测试连接
+    }
+}
