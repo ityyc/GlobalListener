09f4e8a194861123c62bd7b53292eede71c4bcd3
yuanyc <yuanyc@mapbar.com>
2017-06-01 19:25:49 +0800

    chg 添加Butter Knife注解框架

--- app/src/main/java/com/tuba/yuanyc/globallistener/MainActivity.java
+++ app/src/main/java/com/tuba/yuanyc/globallistener/MainActivity.java
@@ -1,15 +1,46 @@
 package com.tuba.yuanyc.globallistener;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * 测试
  */
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button1)
+    Button button1;
+    @BindView(R.id.button2)
+    Button button2;
+    @BindView(R.id.button3)
+    Button button3;
+    @BindView(R.id.button4)
+    Button button4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.button1, R.id.button2, R.id.button3, R.id.button4})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                break;
+            case R.id.button2:
+                break;
+            case R.id.button3:
+                break;
+            case R.id.button4:
+                break;
+        }
     }
 }
